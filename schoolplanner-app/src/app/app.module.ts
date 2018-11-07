ba7657826f328d7f0eaeaa7d27ab5dc81dc3f8a4
Andre Winkler <andre.winkler@web.de>
2018-11-07 21:05:51 +0100

    Some examples for the Material Design Components.

--- schoolplanner-app/src/app/app.module.ts
+++ schoolplanner-app/src/app/app.module.ts
@@ -5,7 +5,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+// Material Design
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatIconModule} from '@angular/material/icon';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
 
 @NgModule({
     declarations: [
@@ -16,7 +24,16 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
         AppRoutingModule,
         FormsModule,
         ReactiveFormsModule,
-        NgbModule
+        // Material Design
+        BrowserAnimationsModule,
+        MatButtonModule,
+        MatCheckboxModule,
+        MatMenuModule,
+        MatIconModule,
+        MatCardModule,
+        MatToolbarModule,
+        MatDividerModule,
+        MatListModule
     ],
     providers: [],
     bootstrap: [AppComponent]
